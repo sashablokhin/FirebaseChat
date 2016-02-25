9c14990e023eb325243150e2729d36695ab6198d
sashablokhin <sashablokhindev@gmail.com>
2016-02-25 14:32:42 +0300

    Added Firebase auth

--- FirebaseChat/LoginViewController.swift
+++ FirebaseChat/LoginViewController.swift
@@ -7,16 +7,66 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet var emailTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
 
+    private let myRootRef = Firebase(url:"https://simple-chatapp.firebaseio.com")
+
     @IBAction func loginButtonPressed(sender: AnyObject) {
+        myRootRef.authUser(emailTextField.text, password: passwordTextField.text,
+            withCompletionBlock: { (error, auth) in
+                if error != nil {
+                    print(error.userInfo["NSLocalizedDescription"]!)
+                }
+        })
     }
     
     @IBAction func signUpButtonPressed(sender: AnyObject) {
+        let alert = UIAlertController(title: "Register",
+            message: "Register",
+            preferredStyle: .Alert)
+        
+        let saveAction = UIAlertAction(title: "Save",
+            style: .Default) { (action: UIAlertAction!) -> Void in
+                
+                let emailField = alert.textFields![0]
+                let passwordField = alert.textFields![1]
+                
+                self.myRootRef.createUser(emailField.text, password: passwordField.text) { (error: NSError!) in
+                    if error == nil {
+                        self.myRootRef.authUser(emailField.text, password: passwordField.text,
+                            withCompletionBlock: { (error, auth) -> Void in
+                        })
+                    }
+                }
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel",
+            style: .Default) { (action: UIAlertAction!) -> Void in
+        }
+        
+        alert.addTextFieldWithConfigurationHandler {
+            (textEmail) -> Void in
+            textEmail.placeholder = "Enter your email"
+            textEmail.keyboardType = .EmailAddress
+        }
+        
+        alert.addTextFieldWithConfigurationHandler {
+            (textPassword) -> Void in
+            textPassword.secureTextEntry = true
+            textPassword.placeholder = "Enter your password"
+        }
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        presentViewController(alert,
+            animated: true,
+            completion: nil)
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -35,6 +85,16 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         
         emailTextField.delegate = self
         passwordTextField.delegate = self
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        myRootRef.observeAuthEventWithBlock { (authData) -> Void in
+            if authData != nil {
+                self.performSegueWithIdentifier("showMessages", sender: nil)
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
